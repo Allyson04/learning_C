036c67b663e85c9d0068e6e423e980f54390f77f
Allyson <allysoneduardo04@gmail.com>
2021-03-22 15:43:52 -0300

    docs: created file for types of data

---
+++ basics/data-types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    //in C, variable's types is a bit different from javascript, but you will see forward it
+
+    char; // stores a character, an word or symbol, does not include 'line breaks' or things similar
+    // uses 1 byte
+
+    int; // stores integer number, or numbers without decimal point
+    // used 2 bytes, now it can be 4 bytes
+
+    float; // stores number with decimal points, but in small precision
+    // like integers, uses 4 bytes
+
+    double; // stores numbers with decimal points too, but with more precision
+    // 'double' of float, so 8 bytes
+}
