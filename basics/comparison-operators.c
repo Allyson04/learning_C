115318a76d9ca20d70118f0bafd174d0ac831963
Allyson <allysoneduardo04@gmail.com>
2021-03-22 19:27:00 -0300

    feat: added comparison operators for C

---
+++ basics/comparison-operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    //in C, comparison operators is the same as for many languages, but let's see each one anyway
+
+    // < "less than"
+    // > "more than"
+    // <= "less or equal as"
+    // >= "more or equal as"
+    // == "equal as"
+    // != "different than"
+
+}
