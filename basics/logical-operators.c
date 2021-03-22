8c0d3e720be3eebd4ff0be973270121366a1f706
Allyson <allysoneduardo04@gmail.com>
2021-03-22 19:28:59 -0300

    feat: added logical operators for C

---
+++ basics/logical-operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    //in C, logical operators is the same as for many languages, but let's see each one anyway
+
+    // && (and), sends "true" when every sentence is true
+    // || (or), sends "true" when any sentence is true
+    // ! (no), sends inverse value
+
+}
