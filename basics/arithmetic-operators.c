536916852b50ecd251ff67e72858d78bea7d3fbe
Allyson <allysoneduardo04@gmail.com>
2021-03-22 20:29:07 -0300

    feat: added arithmetic operators for C

--- basics/arithmetic-operators.c
+++ basics/arithmetic-operators.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
+        //in C, logical operators is also the same as for many languages
+
+        // + (addition) "don't forget that this symbol can also means concatanate"
+        // - (subtraction)
+        // * (multiplication)
+        // '/' (division)
+        // % (module "rest in divisions")
+        // ++ (increment "summing one in value")
+        // -- (decrement "subtracting one in value")
 
 }
