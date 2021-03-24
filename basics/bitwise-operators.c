e8537788492374db6f33f31bb0ee0cc1d26af0b8
Allyson <allysoneduardo04@gmail.com>
2021-03-24 00:23:29 -0300

    feat: added bitwise operator 'and' and an example

---
+++ basics/bitwise-operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    //in C memory operation was crucial for his popularity, that's why we could use bitwise operators
+    // 0 => false | 1 => true
+
+    // & (and)
+    // and example for code using & is checking if a number is even or odd
+    //  37   0   0   1   0   0   1   0   1
+    // & 1   0   0   0   0   0   0   0   1
+    //       -----------------------------
+    //       0   0   0   0   0   0   0   1  
+    // at last, because of the last digit in the first number is 1, we already know that the number is odd
+    // if it was even, it would end as 0
+
+}
