4eae07e5c0387920f8e5e0b6ff87f83a7abef75f
Allyson <allysoneduardo04@gmail.com>
2021-03-24 01:12:58 -0300

    feat: added example for two's complement and what it is

--- basics/bitwise-operators.c
+++ basics/bitwise-operators.c
@@ -28,4 +28,26 @@ int main() {
     // ~37
     // 00100101 => 11011010
 
+    // a good use for this is to discover the two's complement of an number
+    // a two's complement is used to define negative numbers, 37 to -37 or 53 to -53
+    // the two's completement is obtained through inverting all bits (like ~ (not) operator) and adding 1 to first digit
+
+    // 37 to -37
+    //  0   0   1   0   0   1   0   1  
+    //  1   1   0   1   1   0   1   0 (inverted)
+    //  1   1   0   1   1   0   1   1 (added one)
+    // so 011011 is -37, the last digit is used for the signal
+
+    // this way, when we use 37 + (-37)
+    //  0   0   1   0   0   1   0   1
+    //+ 1   1   0   1   1   0   1   1
+    //-------------------------------
+   //1  0   0   0   0   0   0   0   0
+   // because we use only 8 digits, the nineth digit (1) is excluded, and the number results in 0, making a 37 + (-37) = 0 a valid expression
+
+   //also, if you want a shortcut, we can use like this: starting from the rightmost digit, go to the left until a 1 shows up, and then invert all digits to the left of this 1
+   //other tip, we can see if a number is negative of positive based on the leftmost digit, so:
+   // 1 => negative, to use his positive number we do the two's complement of it
+   // 0 => positive 
+
 }
