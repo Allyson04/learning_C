b8c7576052c26d0d5962da600df757634cdd7620
Allyson <allysoneduardo04@gmail.com>
2021-03-25 20:48:05 -0300

    feat: created my first 'hello world' in C

---
+++ basics/hello_world.c
@@ -0,0 +1,7 @@
+//below is an directive of pre-compilation, used to add functions that will be used in main()
+#include <stdio.h>
+
+    //main() is the body of this program, and {} (keys) is where this functions are inserted
+void main() {
+    printf("Hello World!");
+}
